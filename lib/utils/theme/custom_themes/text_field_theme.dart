1938e3ecc258d451962637d889afebf47cbfbbd3
jatinvaira <jatinvaira@gmail.com>
2024-03-22 13:55:20 +0530

    Theme Data

---
+++ lib/utils/theme/custom_themes/text_field_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TTextFormFieldTheme {
+  static final InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+    labelStyle: TextStyle(fontSize: 14, color: Colors.black.withOpacity(0.8)),
+    hintStyle: TextStyle(fontSize: 14, color: Colors.black),
+    floatingLabelStyle: TextStyle(color: Colors.black.withOpacity(0.8)),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: BorderSide(width: 1, color: Colors.grey),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: BorderSide(width: 1, color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: BorderSide(width: 1, color: Colors.red),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: BorderSide(width: 1, color: Colors.red),
+    ),
+  );
+}
