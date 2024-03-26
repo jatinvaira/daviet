fb8b7f6c2c9ff5418d394eebc2acfb2139a64ab8
jatinvaira <jatinvaira@gmail.com>
2024-03-27 01:10:32 +0530

    Forget password interface

---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,45 @@
+import 'package:daviet/common/widgets/login_signup/form_divider.dart';
+import 'package:daviet/common/widgets/login_signup/social_buttons.dart';
+import 'package:daviet/features/authentication/screens/signup/widgets/signup_form.dart';
+import 'package:daviet/utils/constants/sizes.dart';
+import 'package:daviet/utils/constants/text_strings.dart';
+import 'package:daviet/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //         ),
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(TTexts.signupTitle,
+                  style: Theme.of(context).textTheme.headlineMedium),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // form
+              const TSignupForm(),
+              const SizedBox(height: TSizes.spaceBtwInputFields * 2),
+
+              DFormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwInputFields),
+              // Social
+              const DSocialButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
